8cfb9b097c1256d0fb249ea9bc9748b55db4ae00
renzijian <renzijian@meituan.com>
2023-06-08 15:27:15 +0800

    add analysis
    Signed-off-by: renzijian <renzijian@meituan.com>

--- src/main/java/com/xupt/house/controller/admin/AnalysisController.java
+++ src/main/java/com/xupt/house/controller/admin/AnalysisController.java
@@ -1,31 +1,38 @@
 package com.xupt.house.controller.admin;
 
+import com.xupt.house.controller.common.BaseController;
 import com.xupt.house.dto.AnalysizesResult;
 import com.xupt.house.dto.JsonResult;
 import com.xupt.house.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController("/admin/analysis")
+@Controller
 @Slf4j
-public class AnalysisController {
+@RequestMapping(value = "/admin/analysis")
+public class AnalysisController extends BaseController {
 
     @Autowired
     public CategoryService categoryService;
 
     @GetMapping("/categorys")
+    @ResponseBody
     public JsonResult categorys() {
+        System.out.println("hello world");
         AnalysizesResult analysizesResult = new AnalysizesResult();
         analysizesResult.setCategorys(categoryService.findAll());
         int count = analysizesResult.getCategorys().size();
-        List<Double> scores = new ArrayList<>(count);
+        List<Double> scores = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            scores.set(i, (double) (100 / count));
+            scores.add((double) (100 / count));
         }
         analysizesResult.setScores(scores);
         return JsonResult.success("success", analysizesResult);
